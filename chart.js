57cce8caeb0098bd2ba845dfc31fe62020c0d04c
sadafpartovi <74632037+sadafpartovi@users.noreply.github.com>
2021-01-07 15:21:29 -0800

    adding functions

--- chart.js
+++ chart.js
@@ -1,9 +1,12 @@
     
     const alertBanner = document.getElementById("alert");
-//    const trafficCanves = document.getElementById('traffic-chart');
-const dailyChart = document.getElementById('daily-chart');
-const mobileCanvas = document.getElementById('mobile-chart');
-   
+   const trafficCanves = document.getElementById('traffic-chart');
+    const dailyChart = document.getElementById('daily-chart');
+    const mobileCanvas = document.getElementById('mobile-pie');
+    const user = document.getElementById("userField");
+    const message = document.getElementById("messageField");
+    const send = document.getElementById("send");
+    
    
    
    // Alert
@@ -27,7 +30,46 @@ alertBanner.addEventListener("click", e =>{
 
 
 
-// Daily Traffic Chart
+
+
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    }]
+    };
+    let trafficOptions = {
+        aspectRatio: 2.5,
+        animation: {
+        duration: 0
+        },
+        scales: {
+        yAxes: [{
+        ticks: {
+        beginAtZero:true
+        }
+        }]
+        },
+        legend : {
+        display: false
+        }
+        };
+        let trafficChart = new Chart(trafficCanves, {
+            type: 'line',
+            data: trafficData,
+            options: trafficOptions
+            });
+
+
+
+
+
+            // Daily Traffic Chart
 
 
 
@@ -80,6 +122,8 @@ const mobileData = {
         position: 'right',
         labels: {
         boxWidth: 20,
+        
+        
         fontStyle: 'bold'
         }
         }
@@ -88,5 +132,36 @@ const mobileData = {
             type: 'doughnut',
             data: mobileData,
             options: mobileOptions
+            
             });
 
+
+            // user msg
+
+            send.addEventListener("click", () => {
+                if  (user.value === "" && message.value === "") {
+                    window.alert("Please fill out user and message fields before sending");
+                } else if (user.value === "") {
+                    window.alert("Please fill out user field before sending"); 
+                } else if (message.value === "") {
+                    window.alert("Please fill out message field before sending");
+                } else {
+                    window.alert(`Message successfully sent to: ${user.value}`);
+                    user.value = "";
+                    message.value = "";
+                }
+
+            });
+
+            /* INITIALIZING PAGE */
+
+//     function init() {
+// 	    search.value = '';
+// 	    user.value = '';
+// 	    message.value = '';
+// 	    trafficFunction(hourly);
+
+// }
+
+
+// init();
